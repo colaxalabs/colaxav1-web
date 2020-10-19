bde46e0c79e214d7491c4162caff3f5e151452a3
3dw1nM0535 <workockmoses@gmail.com>
2020-10-19 20:15:46 +0300

    initial commit

---
+++ craco.config.js
@@ -0,0 +1,18 @@
+const CracoLessPlugin = require('craco-less')
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: { '@primary-color': '#7546C9' },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
+
