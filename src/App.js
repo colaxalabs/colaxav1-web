eb00357a2272460a0a21befc1750ab6f37889dca
3dw1nM0535 <workockmoses@gmail.com>
2020-10-26 13:00:59 +0300

    feat:loadWeb3

--- src/App.js
+++ src/App.js
@@ -11,8 +11,10 @@ import { Homepage } from './components/pages'
 // Redux actions
 import {
   networkFound,
-  walletFound,
 } from './actions'
+
+// Utils
+import { loadWeb3 } from './utils'
 
 // Redux store
 import store from './store'
@@ -24,29 +26,6 @@ function App() {
   const isMetamaskInstalled = typeof window.ethereum !== 'undefined'
 
   useEffect(() => {
-    async function loadWeb3() {
-      const networkInfo = {}
-      const walletInfo = {}
-      if (isMetamaskInstalled) {
-        window.web3 = new Web3(window.ethereum)
-        networkInfo.currentNetwork = await window.web3.eth.net.getId()
-        walletInfo.isMetamask = window.web3.currentProvider.isMetaMask === undefined ? false : window.web3.currentProvider.isMetaMask
-        store.dispatch(networkFound({ ...networkInfo }))
-        store.dispatch(walletFound({ ...walletInfo }))
-      } else if (window.web3) {
-        window.web3 = new Web3(window.web3.currentProvider)
-        networkInfo.currentNetwork = await window.web3.eth.net.getId()
-        walletInfo.isMetamask = window.web3.currentProvider.isMetaMask === undefined ? false : window.web3.currentProvider.isMetaMask
-        store.dispatch(networkFound({ ...networkInfo }))
-        store.dispatch(walletFound({ ...walletInfo }))
-      } else {
-        window.web3 = new Web3('ws://localhost:8545')
-        networkInfo.currentNetwork = await window.web3.eth.net.getId()
-        walletInfo.isMetamask = window.web3.currentProvider.isMetaMask === undefined ? false : window.web3.currentProvider.isMetaMask
-        store.dispatch(networkFound({ ...networkInfo }))
-        store.dispatch(walletFound({ ...walletInfo }))
-      }
-    }
     loadWeb3()
   })
 
